0ee28f00cad819159e9500de3c8222a876255976
ferares <fermin@ares.uy>
2023-06-06 09:02:51 -0300

    Tunning config prompts

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -17,7 +17,7 @@ class ChatController {
   static async messagePost(req: Request, res: Response) {
     const { message } = req.body
     if (!message) return res.sendStatus(400)
-    const response = await OpenAIService.sendMessage(message, true)
+    const response = await OpenAIService.sendMessage(message, false)
     res.json(response)
   }
 }
--- src/services/openAI.service.ts
+++ src/services/openAI.service.ts
@@ -9,6 +9,14 @@ class OpenAIService {
   private static instance: OpenAIApi | undefined
   private static messages: ChatCompletionRequestMessage[] = [
     { role: 'system', content: 'You are a sales person for SalamHello, a Morrocan rug store' },
+    { role: 'system', content: 'Your name is Stan S. Stanman' },
+    { role: 'system', content: 'The store offers in stock rugs as well as custom rugs tailored to the client\'s taste' },
+    { role: 'system', content: 'For custom rugs the clients can select the size, color and tassels of the rug' },
+    { role: 'system', content: 'Custom rugs usally take between 8 to 10 weeks to produce and ship' },
+    { role: 'system', content: 'For custom rugs larger than  9\' x 12\' the production may take up between 10 to 13 weeks' },
+    { role: 'system', content: 'Do not fabricate information that is not explicitly provided to you regarding the store or its products' },
+    { role: 'system', content: 'If you do not have enough explicitly provided information to respond just apologize' },
+    { role: 'system', content: 'Respond as succinctly as possible' },
     { role: 'assistant', content: 'Hello there, wanna buy some rugs?' },
   ]
 
