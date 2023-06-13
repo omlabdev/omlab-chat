968734ab3aab2d1a729f43bd3e728cdb72e94a41
ferares <fermin@ares.uy>
2023-06-13 09:55:12 -0300

    Separate admin & chat js

---
+++ src/public/main.js
@@ -0,0 +1,69 @@
+function enableLinks(text) {
+  const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z\d+&@#/%?=~_|!:,.;]*[-A-Z\d+&@#/%=~_|])/ig
+  return text.replace(urlRegex, (url) => {
+    return `<a href="${url}" target="_blank" rel="noopener noreferrer nofollow">${url}</a>`
+  })
+}
+
+async function getAllMessages() {
+  setLoader(true)
+  let messages
+  try {
+    const response = await fetch('/json', { method: 'get' })
+    messages = await response.json()
+  } catch (error) {
+    messages = [{ role: 'error', content: 'There was an error processing your message, please try again' }]
+    console.error(error)
+  }
+  for (const message of messages) {
+    addMessage(message)
+  }
+  setLoader(false)
+}
+
+function setLoader(status) {
+  const loader = document.querySelector('[js-loader]')
+  if (!loader) return
+  if (status) loader.classList.add('show')
+  else loader.classList.remove('show')
+}
+
+function createMessage(message) {
+  const messageElement = document.createElement('li')
+  messageElement.classList.add('message')
+  messageElement.classList.add(`message--${message.role}`)
+  messageElement.innerHTML = message.content.replaceAll('\n', '<br>')
+  return messageElement
+}
+
+function addMessage(message) {
+  message.content = enableLinks(message.content)
+  const messagesList = document.querySelector('[js-messages]')
+  const messagesWrapper = document.querySelector('[js-messages-wrapper]')
+  if (!messagesList) return
+  const messageElement = createMessage(message)
+  messagesList.appendChild(messageElement)
+  if (messagesWrapper) messagesWrapper.scrollTop = messagesWrapper.scrollHeight
+  return messageElement
+}
+
+async function sendMessage() {
+  const inputElement = document.querySelector('[js-input]')
+  const messages = document.querySelector('[js-messages]')
+  if ((!inputElement) || (!messages)) return
+  setLoader(true)
+  const input = inputElement.value
+  inputElement.value = ''
+  addMessage({ role: 'user', content: input })
+  const body = JSON.stringify({ message: input })
+  let message
+  try {
+    const response = await fetch('/', { method: 'post', headers: { 'content-type': 'application/json' }, body })
+    message = await response.json()
+  } catch (error) {
+    message = { role: 'error', content: 'There was an error processing your message, please try again' }
+    console.error(error)
+  }
+  addMessage(message)
+  setLoader(false)
+}
