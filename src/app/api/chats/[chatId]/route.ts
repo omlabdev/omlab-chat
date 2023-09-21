f11de1a476fe23fc47b2f80e467c303bc4864aee
ferares <fermin@ares.uy>
2023-09-21 10:17:53 -0300

    Add missing DB connection to route

--- src/app/api/chats/[chatId]/route.ts
+++ src/app/api/chats/[chatId]/route.ts
@@ -2,11 +2,14 @@ import { NextRequest, NextResponse } from 'next/server'
 
 import Chat from '@/models/chat'
 
+import Database from '@/services/database.service'
+
 export const dynamic = 'force-dynamic' // Don't pre-render this GET method
 
 export async function GET(_: NextRequest, { params }: { params: { chatId: string } }) {
   const { chatId } = params
   if (!chatId) return NextResponse.json({}, { status: 400 })
+  await Database.connect()
   const chat = await Chat.findOne({ chatId }).lean().exec()
   if (!chat) return NextResponse.json({}, { status: 404 })
   return NextResponse.json(chat)
