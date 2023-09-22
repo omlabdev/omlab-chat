4354dac790b73c759095d6751c49cfeea4f6089c
ferares <fermin@ares.uy>
2023-09-22 11:53:01 -0300

    Fix fonts

--- src/components/theme.tsx
+++ src/components/theme.tsx
@@ -1,12 +1,18 @@
 import { Chat } from '@/models/chat'
 
+async function loadFontFace(fontFace: FontFace) {
+  const loadedFont = await fontFace.load()
+  document.fonts.add(loadedFont)
+}
+
 export default function Theme({ chat }: { chat?: Chat }) {
   if (!chat) return null
+  if (chat.font) loadFontFace(new FontFace(chat.name, `url("${chat.font}")`, { display: 'swap', style: 'normal', weight: 'normal' }))
   return (
     <>
       {chat.colors?.main && (<style>{`.theme{--color-main:${chat.colors.main}}`}</style>)}
       {chat.colors?.background && (<style>{`.theme{--color-background:${chat.colors.background}}`}</style>)}
-      {chat.font && (<style>{`.theme{--font-family-text:${chat.font}}`}</style>)}
+      {chat.font && (<style>{`.theme{--font-family-text:'${chat.name}'}`}</style>)}
     </>
   )
 }
