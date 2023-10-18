55350e76d071aa39193cd75a148e18bc55d794a7
ferares <fermin@ares.uy>
2023-10-18 09:53:07 -0300

    Add Om Lab functions

--- src/services/functions.service.ts
+++ src/services/functions.service.ts
@@ -2,55 +2,51 @@ import { Function as ChatFunction } from '@/types'
 
 declare type ChatFunctionWithReference = { function: Function, async: boolean, chatFunction: ChatFunction }
 
-export const functions: ChatFunctionWithReference[] = [
-  {
-    function: salamQueryProducts,
-    async: false,
-    chatFunction: {
-      name: 'salamQueryProducts',
-      description: 'Retrive information about products that match the given parameters',
-      parameters: {
-        type: 'object',
-        properties: {
-          color: { type: 'string', enum: ['neutral', 'black-white', 'brown', 'blue', 'green', 'grey', 'orange', 'pink', 'purple', 'red', 'yellow'] },
-          size: { type: 'string', enum: ['accent', 'runner', 'area-rugs', 'oversized', 'wall-decor', 'pillows'] },
-          use: { type: 'string', enum: ['living-room', 'dining-room', 'bedroom', 'hallway', 'wall-decor', 'kitchen'] },
-          pile: { type: 'string', enum: ['flat', 'low-hand-knot', 'medium-to-high', 'mixed'] },
-          technique: { type: 'string', enum: ['flatweave', 'kharita-tazenakht', 'hanbel', 'low-hand-knot', 'medium-to-high-hand-knot', 'zanafi', 'technique_boucherouite'] },
-          style: { type: 'string', enum: ['abstract', 'checkered', 'maximalist', 'minimalist', 'modern', 'traditional', 'trellis', 'vintage'] },
-        },
-        required: [],
-      },
-    },
-  },
-  {
-    function: salamData,
-    async: false,
-    chatFunction: {
-      name: 'salamData',
-      description: 'Retrive general information about Salam Hello specified by topic of interest',
-      parameters: {
-        type: 'object',
-        properties: {
-          topic: { type: 'string', enum: [
-            'shipping_locations',
-            'shipping_costs',
-            'shipping_times',
-            'sales',
-            'outlet',
-            'services',
-            'history',
-            'founder',
-            'mission',
-            'contact',
-            'newsletter',
-          ] },
-        },
-        required: ['topic'],
-      },
-    },
-  },
-]
+/* ***********************
+/*   Om Lab functions    *
+************************ */
+
+function omLabData({ topic }: { topic: string}) {
+  switch (topic) {
+    case 'services':
+      return 'Om Lab does software development and specializes in AI for e-commerce and CRO. Their most recent product is a ChatGPT-powered 24/7 sales agent.'
+    case 'technologies':
+      return 'We work with Shopify, Big Commerce, WordPress/Woocommerce, Next.js/React, Python/Django and many more.'
+    case 'cro':
+      return 'Optimize your Conversion Rate with our Customer-behavior-based A/B testing process.'
+    case 'ecommerce':
+      return 'Get the most out of your Store with our 360º e‑commerce optimization process.'
+    case 'ai':
+      return 'Increase your engagement with our ChatGPT-powered 24/7 sales agent.'
+    case 'contact':
+      return 'You can schedule a meeeting with us here https://calendly.com/rafael-146/discovery'
+    default:
+      return 'Invalid topic provided.'
+  }
+}
+
+function omLabQuestions({ question }: { question: string}) {
+  switch (question) {
+    case 'how_chatbot_interacts_with_customers':
+      return 'Our chatbot uses AI to interact in real-time with customers through your website, e‐commerce platform, and social media channels. It can address queries, suggest products, and offer an exceptional user experience.'
+    case 'how_can_chatbot_increase_sales':
+      return 'The chatbot not only provides assistance but can also suggest products based on customer preferences, which can boost your sales.'
+    case 'platform_integrations':
+      return 'Our chatbot is designed to seamlessly integrate with your existing e‐commerce and CRM platforms, enhancing the coherence and efficiency of your digital ecosystem.'
+    case 'what_is_chatbot':
+      return 'A Chatbot is a virtual assistant programmed to interact with your customers automatically and in a personalized manner. By employing advanced AI algorithms, our Chatbot can learn and adapt to your customers\' needs, providing quick and accurate responses 24/7.'
+    case 'how_can_chatbot_help':
+      return 'Providing fast and accurate answers to your Clients. Our AI-Sales Agent chatbot is capable of learnin and adapt to your Customer’s needs and porfiles, providing effective answers of your products and services 24/7. Increasing engagement and driving conversion.'
+    case 'why_use_chatbot':
+      return 'The conversational Sales Agent improves the experience of those who visit your e‐commerce, immediately answering queries through a personalized and unique conversation, assisting customers 24/7/365.'
+    default:
+      return 'Invalid question provided.'
+  }
+}
+
+/* ***********************
+/* Salam Hello functions *
+************************ */
 
 function salamData({ topic }: { topic: string}) {
   switch (topic) {
@@ -84,6 +80,78 @@ function salamQueryProducts(args: { color?: string, size?: string, use?: string,
   const tags = [color, size, use, pile, technique, style].filter((tag) => !!tag).join('+')
   return `Products that match your query: https://salamhello.com/collections/all/${tags}`
 }
+
+/* ***********************
+/*  Function definitions *
+************************ */
+
+export const functions: ChatFunctionWithReference[] = [
+  {
+    function: salamQueryProducts,
+    async: false,
+    chatFunction: {
+      name: 'salamQueryProducts',
+      description: 'Retrive information about products that match the given parameters',
+      parameters: {
+        type: 'object',
+        properties: {
+          color: { type: 'string', enum: ['neutral', 'black-white', 'brown', 'blue', 'green', 'grey', 'orange', 'pink', 'purple', 'red', 'yellow'] },
+          size: { type: 'string', enum: ['accent', 'runner', 'area-rugs', 'oversized', 'wall-decor', 'pillows'] },
+          use: { type: 'string', enum: ['living-room', 'dining-room', 'bedroom', 'hallway', 'wall-decor', 'kitchen'] },
+          pile: { type: 'string', enum: ['flat', 'low-hand-knot', 'medium-to-high', 'mixed'] },
+          technique: { type: 'string', enum: ['flatweave', 'kharita-tazenakht', 'hanbel', 'low-hand-knot', 'medium-to-high-hand-knot', 'zanafi', 'technique_boucherouite'] },
+          style: { type: 'string', enum: ['abstract', 'checkered', 'maximalist', 'minimalist', 'modern', 'traditional', 'trellis', 'vintage'] },
+        },
+        required: [],
+      },
+    },
+  },
+  {
+    function: salamData,
+    async: false,
+    chatFunction: {
+      name: 'salamData',
+      description: 'Retrive general information about Salam Hello specified by topic of interest',
+      parameters: {
+        type: 'object',
+        properties: {
+          topic: { type: 'string', enum: ['shipping_locations', 'shipping_costs', 'shipping_times', 'sales', 'outlet', 'services', 'history', 'founder', 'mission', 'contact', 'newsletter'] },
+        },
+        required: ['topic'],
+      },
+    },
+  },
+  {
+    function: omLabData,
+    async: false,
+    chatFunction: {
+      name: 'omLabData',
+      description: 'Retrive general information about Om Lab specified by topic of interest',
+      parameters: {
+        type: 'object',
+        properties: {
+          topic: { type: 'string', enum: ['services', 'technologies', 'cro', 'ecommerce', 'ai', 'contact'] },
+        },
+        required: ['topic'],
+      },
+    },
+  },
+  {
+    function: omLabQuestions,
+    async: false,
+    chatFunction: {
+      name: 'omLabQuestions',
+      description: 'Answer FAQs about Om Lab\'s AI-Sales Agent',
+      parameters: {
+        type: 'object',
+        properties: {
+          question: { type: 'string', enum: ['how_chatbot_interacts_with_customers', 'how_can_chatbot_increase_sales', 'platform_integrations', 'what_is_chatbot', 'how_can_chatbot_help', 'why_use_chatbot'] },
+        },
+        required: ['question'],
+      },
+    },
+  },
+]
 
 export default class FunctionsService {
   static getFunction(name: string) {
